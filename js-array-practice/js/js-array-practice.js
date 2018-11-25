9d5c10ffb395b25a3d626b72fe51984e54eb7aa9
Grace <lunay0325@gmail.com>
2018-11-25 19:10:31 +0800

    Add a function that can multiply every item in an array by 2

--- js-array-practice/js/js-array-practice.js
+++ js-array-practice/js/js-array-practice.js
@@ -1,8 +1,19 @@
-const A = '[1,2,3,4]', B = [1,2,3,4];
+//Task1: judge whether the following two variables are array or not.
+const A = '[1,2,3,4]',
+  B = [1, 2, 3, 4];
 
 function isArray(obj) {
-    return obj instanceof Array;
+  return obj instanceof Array;
 }
-
 console.log(isArray(A));
 console.log(isArray(B));
+//Task2: multiply every item in the following array by 2.
+const C = [1, 2, 3, 4, 5];
+
+function multiplyByTwo(array) {
+  return array.map(function(item, index, array) {
+    return (item * 2);
+  })
+}
+
+console.log(multiplyByTwo(C));
