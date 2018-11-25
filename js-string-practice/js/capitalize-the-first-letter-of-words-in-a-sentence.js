aac994ad33853086f5c4380974b45ab5ff08ca6b
Grace <lunay0325@gmail.com>
2018-11-25 17:16:55 +0800

    Modify the format of the js file

--- js-string-practice/js/capitalize-the-first-letter-of-words-in-a-sentence.js
+++ js-string-practice/js/capitalize-the-first-letter-of-words-in-a-sentence.js
@@ -1,12 +1,12 @@
 const SENTENCE = "good afternoon, mr mike.";
 
 function capitalizeTheFirstLetterOfWordsInTheSentence(sentence) {
-    let words = sentence.split(" ");
-    let result = "\n";
-    for(let word of words) {
-        let capitalizedWord = word[0].toUpperCase() + word.substr(1) + " ";
-        result += capitalizedWord;
-    }
-    return result;
+  let words = sentence.split(" ");
+  let result = "\n";
+  for (let word of words) {
+    let capitalizedWord = word[0].toUpperCase() + word.substr(1) + " ";
+    result += capitalizedWord;
+  }
+  return result;
 }
 document.getElementById("output").innerHTML += capitalizeTheFirstLetterOfWordsInTheSentence(SENTENCE);
