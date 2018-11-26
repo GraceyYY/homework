ff9097ccb386f06301b43b3a75005c94c314540b
Grace <lunay0325@gmail.com>
2018-11-27 07:57:56 +0800

    Edit the name of variables

--- js-array-practice/js/js-array-practice.js
+++ js-array-practice/js/js-array-practice.js
@@ -1,21 +1,21 @@
 //Task1: judge whether the following two variables are array or not.
-const A = '[1,2,3,4]',
-  B = [1, 2, 3, 4];
+const TO_BE_TEST_A = '[1,2,3,4]',
+  TO_BE_TEST_B = [1, 2, 3, 4];
 
 function isArray(obj) {
   return obj instanceof Array;
 }
-console.log(isArray(A));
-console.log(isArray(B));
+console.log(isArray(TO_BE_TEST_A));
+console.log(isArray(TO_BE_TEST_B));
 //Task2: multiply every item in the following array by 2.
-const C = [1, 2, 3, 4, 5];
+const TO_BE_MULTIPLIED = [1, 2, 3, 4, 5];
 
 function multiplyByTwo(array) {
   return array.map(function(item, index, array) {
     return (item * 2);
   })
 }
-console.log(multiplyByTwo(C));
+console.log(multiplyByTwo(TO_BE_MULTIPLIED));
 //Task3: join the items of the array with "," , "+" and space.
 const COLORS = ["Red", "Green", "White", "Black"];
 
@@ -26,7 +26,7 @@ function joinTheArray() {
 }
 joinTheArray();
 //Task4: sort the array by number in descending order.
-const D = [5, 1, 8, 10, 4];
+const TO_BE_SORTED = [5, 1, 8, 10, 4];
 
 function compare(val1, val2) {
   if (val1 < val2) {
@@ -37,4 +37,8 @@ function compare(val1, val2) {
     return 0;
   }
 }
-console.log(D.sort(compare));
+console.log(TO_BE_SORTED.sort(compare));
+//Task5: find the item that appears most in the array.
+const CONTENT = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
+function findAppearsTheMost()
