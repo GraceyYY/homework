4b65e0862df28ea0e68dc9474d456102df04b579
Grace <lunay0325@gmail.com>
2018-11-27 08:58:21 +0800

    Edit all files name to meet the standard

--- js_string_practice/js/capitalize_characters.js
+++ js_string_practice/js/capitalize_characters.js
@@ -1,3 +1,14 @@
+const NAME = "hello";
+
+function capitalizeAllCharacters(string) {
+  let result = "";
+  for (let character of string) {
+    result += character.toUpperCase();
+  }
+  return result;
+}
+document.getElementById("output").innerHTML = capitalizeAllCharacters(NAME);
+
 const SENTENCE = "good afternoon, mr mike.";
 
 function capitalizeTheFirstLetterOfWordsInTheSentence(sentence) {
