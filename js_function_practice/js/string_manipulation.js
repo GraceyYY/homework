6b9d2d9db00c40a48a5160ad88eaf6ebf24ca3d9
Grace <lunay0325@gmail.com>
2018-11-30 16:22:32 +0800

    Add a function that sort characters of a string in the alphabet order

--- js_function_practice/js/string_manipulation.js
+++ js_function_practice/js/string_manipulation.js
@@ -4,7 +4,12 @@ function reverseString(message) {
 console.log(reverseString('hello'));
 
 function palindrome(message) {
-    return message === reverseString(message);
+  return message === reverseString(message);
 }
 console.log(palindrome('hello'));
 console.log(palindrome('abcba'));
+
+function alphabetSort(message) {
+  return message.split('').sort().join('');
+}
+console.log(alphabetSort('hello'));
