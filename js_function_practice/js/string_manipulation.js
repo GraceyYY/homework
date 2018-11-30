d50ff7c243a76cc45deeb2009591fffb2ef1fe82
Grace <lunay0325@gmail.com>
2018-11-30 16:18:09 +0800

    Add a function that can judge if the string is a palindrome

--- js_function_practice/js/string_manipulation.js
+++ js_function_practice/js/string_manipulation.js
@@ -2,3 +2,9 @@ function reverseString(message) {
   return message.split('').reverse().join('');
 }
 console.log(reverseString('hello'));
+
+function palindrome(message) {
+    return message === reverseString(message);
+}
+console.log(palindrome('hello'));
+console.log(palindrome('abcba'));
