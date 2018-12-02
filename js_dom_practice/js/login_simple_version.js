74d9dcde8380de21f208fe3fd60a54cbf56973e0
Grace <lunay0325@gmail.com>
2018-12-02 16:47:37 +0800

    Reconstruct the main function

--- js_dom_practice/js/login_simple_version.js
+++ js_dom_practice/js/login_simple_version.js
@@ -1,60 +1,57 @@
-function check() {
-  let username = document.getElementById('username');
-  let password = document.getElementById('password');
-  let errMsg = document.querySelector('p.err-msg');
-  let loginMsg = document.querySelector('p.login-msg');
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let errMsg = document.querySelector('p.err-msg');
+let loginMsg = document.querySelector('p.login-msg');
+
+function isEmpty() {
+  if (username.value != '' && password.value != '') {
+    return false;
+  } else {
+    showErr('empty');
+    isLoginSuccess(false);
+    return true;
+  }
+}
+
+function checkLogin() {
   clearErrMsg();
-  checkLogin();
-
-  function isEmpty() {
-    if (username.value != '' && password.value != '') {
-      return false;
+  if (!isEmpty()) {
+    if (username.value === '123' && password.value === '123') {
+      isLoginSuccess(true);
     } else {
-      showErr('empty');
+      clearPassword();
+      showErr('error');
       isLoginSuccess(false);
-      return true;
     }
   }
+}
 
-  function checkLogin() {
-    if (!isEmpty()) {
-      if (username.value === '123' && password.value === '123') {
-        isLoginSuccess(true);
-      } else {
-        clearPassword();
-        showErr('error');
-        isLoginSuccess(false);
-      }
-    }
-  }
+function clearErrMsg() {
+  errMsg.innerText = '';
+  loginMsg.innerText = '';
+}
 
-  function clearErrMsg() {
-    errMsg.innerText = '';
-    loginMsg.innerText = '';
+function showErr(err) {
+  switch (err) {
+    case 'error':
+      errMsg.innerText = '用户名或密码错误';
+      break;
+    case 'empty':
+      errMsg.innerText = '用户名或密码不能为空';
+      break;
+    default:
+      clearErrMsg();
   }
+}
 
-  function showErr(err) {
-    switch (err) {
-      case 'error':
-        errMsg.innerText = '用户名或密码错误';
-        break;
-      case 'empty':
-        errMsg.innerText = '用户名或密码不能为空';
-        break;
-      default:
-        clearErrMsg();
-    }
+function isLoginSuccess(state) {
+  if (state) {
+    loginMsg.innerText = '登录成功';
+  } else {
+    loginMsg.innerText = '登录失败';
   }
+}
 
-  function isLoginSuccess(state) {
-    if (state) {
-      loginMsg.innerText = '登录成功';
-    } else {
-      loginMsg.innerText = '登录失败';
-    }
-  }
-
-  function clearPassword() {
-    password.value = '';
-  }
+function clearPassword() {
+  password.value = '';
 }
